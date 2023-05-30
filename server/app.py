c637bfbbd6e7c0c805b187cfbbe2f808c4524cd9
John Truong <viajohnt@gmail.com>
2023-05-29 20:00:19 -0600

    Final Commit

--- server/app.py
+++ server/app.py
@@ -31,7 +31,7 @@ class IndexArticle(Resource):
     
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
-        return articles, 200
+        return make_response(jsonify(articles), 200)
 
 class ShowArticle(Resource):
 
@@ -46,12 +46,50 @@ class ShowArticle(Resource):
 
             return make_response(article_json, 200)
 
-        return {'message': 'Maximum pageview limit reached'}, 401
+        return make_response(
+            jsonify({
+                'message': 'Maximum pageview limit reached'
+            }),
+            401
+        )
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+
+    def post(self):
+        
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+
+        session['user_id'] = user.id
+
+        return user.to_dict(), 200
+
+class Logout(Resource):
+
+    def delete(self):
+
+        session['user_id'] = None
+        
+        return {}, 204
+
+class CheckSession(Resource):
+
+    def get(self):
+        
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        
+        return {}, 401
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
